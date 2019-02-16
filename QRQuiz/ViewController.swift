c00ded72a02f319187048ea57d83aedc46daeef6
shocho0101 <shocho0101@hotmail.co.jp>
2019-02-16 13:12:04 +0900

    Initial Commit

---
+++ QRQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRQuiz
+//
+//  Created by 張翔 on 2019/02/16.
+//  Copyright © 2019 張翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
